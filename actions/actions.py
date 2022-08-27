a702533c466cef9af7adf82414cbe0eea160fbe0
Jaishma Usha <jaishmarajan8@gmail.com>
2022-08-26 20:52:30 -0400

    first commit

---
+++ actions/actions.py
@@ -0,0 +1,64 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+from datetime import date
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+#
+import pandas as pd
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+class ActionAdmissionInfo(Action):
+
+    def name(self) -> Text:
+        return "action_admission_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        str((tracker.latest_message)['text'])
+        dispatcher.utter_message(template="utter_admission_info")
+
+        return []
+
+class DisplayUpcomingHolidays(Action):
+
+    def name(self) -> Text:
+        return "display_upcoming_holidays"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        today = date.today()
+        print("Today's date:", today)
+        this_month = today.strftime("%m")
+        df2 = pd.read_excel('Calender_2022.xlsx')
+        df2['Date'] = pd.to_datetime(df2['Date'])
+        current_month_df = df2[df2['Date'].dt.month == int(this_month)]
+        content = 'Total of ' + str(current_month_df.shape[0]) + ' this month\n\n'
+        for i in range(current_month_df.shape[0]):
+            content = content + str(current_month_df['Date'].values[i])[:10] + '  -  ' + str(
+                current_month_df['Holiday Description'].values[i]) + '\n'
+
+        dispatcher.utter_message(text=content)
+
+        return []
